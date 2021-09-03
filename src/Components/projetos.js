f09efcb36188f9f034995a7114e028aaac3caa7a
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-09-03 16:24:51 -0300

    Redirecting to login if tried to post project

--- src/Components/projetos.js
+++ src/Components/projetos.js
@@ -2,13 +2,14 @@ import '../App.css'
 import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 import { Link } from "react-router-dom";
+import { useAuthState } from '../Context';
 
 
-function Projetos(){
+function Projetos() {
     useEffect(() => {
         getProjetos();
     }, [])
-
+    const state = useAuthState();
     const [projetos, setProjetos] = useState([]);
 
     const getProjetos = () => {
@@ -32,10 +33,10 @@ function Projetos(){
             <div className="cabecalho">
                 <h1>Projetos</h1>
                 <div className="link" >
-                    <a href='/cadastrarprojetos' id='cadastrarprojetos'  > Adicione seu projeto! </a>{' '}
+                    <a href={state.userDetails ? '/cadastrarprojetos' : '/login'} id='cadastrarprojetos'  > Adicione seu projeto! </a>{' '}
                 </div>
                 <div id='selecaoCampus'>
-                    <input id='selecao' list='Campus' autocomplete="off" placeholder="Escolha o Campus"/>
+                    <input id='selecao' list='Campus' autocomplete="off" placeholder="Escolha o Campus" />
                     <datalist id='Campus' >
                         <option id='opcao' value='Garanhuns'>Garanhuns</option>
                         <option id='opcao' value='Arcoverde'>Arcoverde</option>
@@ -44,18 +45,18 @@ function Projetos(){
             </div>
             <div id='lista'>
                 <ul>
-                    {projetos.map(projeto => (   
+                    {projetos.map(projeto => (
                         <li id='projeto' className="projeto" key={projeto.id}>
-                            <Link to={`/projetos/${projeto.id}` }>
+                            <Link to={`/projetos/${projeto.id}`}>
                                 <div >
                                     <h3>{projeto.titulo}</h3>
                                 </div>
                             </Link>
-                        </li>             
+                        </li>
                     ))}
                 </ul>
             </div>
-            
+
         </div>
     )
 }
